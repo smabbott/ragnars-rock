549ee9a93622ebddcbbc9685b89b654916c063a5
Seth Mabbott <seth.mabbott@gmail.com>
2014-11-25 12:51:13 -0600

    style some things a bit including buttons, admin, form elements. attempt to generate greyscale thumbnail images with no luck

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -5,12 +5,6 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # storage :file
   storage :fog
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
-  # def store_dir
-  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  # end
-
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
@@ -35,16 +29,29 @@ class PhotoUploader < CarrierWave::Uploader::Base
     process :resize_to_fill => [300, 100]
   end
 
+  version :thumb_wide_grey do
+    process :grey_version
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+private
+ 
+  # FIXME: this doesn't appear to work
+  def grey_version
+    manipulate! do |img|
+      img.combine_options do |c|
+        c.trim
+        c.resize      "#{300}x#{100}>"
+        c.resize      "#{300}x#{100}<"
+        c.quantize(256, Magick::GRAYColorspace)
+      end
+      img
+    end
+  end
 
 end
